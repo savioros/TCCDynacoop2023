b2b6649ad75cfb36b8ea0216a8475d3026e88030
savio <saviopeck@outlook.com>
2023-03-09 17:24:29 -0300

    Add project files.

---
+++ TCCDynacoop2023.Logistics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCCDynacoop2023.Logistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
