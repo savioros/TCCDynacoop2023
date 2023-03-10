3deaa1bdc6a8a656400c177fe1e348e0c06efefc
savio <saviopeck@outlook.com>
2023-03-10 17:28:06 -0300

    Plugin que não permite criação de produtos no ambiente 2

---
+++ Dynacoop2023.Logistics.MyFirstPlugins/Plugins/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using Dynacoop2023.Logistics.MyFirstPlugins.DynacoopISV;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynacoop2023.Logistics.MyFirstPlugins.Plugins
+{
+    public class ProductManager : PluginCore
+    {
+        protected override void ExecutePlugin(IServiceProvider serviceProvider)
+        {
+            if (this.Context.MessageName == "Create") throw new InvalidPluginExecutionException("The product cannot be registered in this environment");
+        }
+    }
+}
