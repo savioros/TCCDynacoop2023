57fe7def7a78c0816f146ff6ac491ebb7ba2d999
savio <saviopeck@outlook.com>
2023-03-10 16:31:32 -0300

    Clonagem da oportunidade

---
+++ Dynacoop2023.Logistics.MyFirstPlugins/Actions/Cloneopportunity.cs
@@ -0,0 +1,54 @@
+﻿using Dynacoop2023.Logistics.MyFirstPlugins.DynacoopISV;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace Dynacoop2023.Logistics.MyFirstPlugins.Actions
+{
+    public class Cloneopportunity : ActionCore
+    {
+        [Input("OpportunityId")]
+        public InArgument<string> OpportunityId { get; set; }
+
+        [Output("NewOpportunityId")]
+        public OutArgument<string> NewOpportunityId { get; set; }
+
+        public override void ExecuteAction(CodeActivityContext context)
+        {
+            try
+            {
+                Entity opportunity = GetOpportunity(context);
+                string newOpportunityId = CreateOpportunityClone(opportunity).ToString();
+
+                this.NewOpportunityId.Set(context, newOpportunityId);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error Action Clone Opportunity: " + e.Message);
+            }
+        }
+
+        private Guid CreateOpportunityClone(Entity opportunity)
+        {
+            Entity newOpportunity = new Entity("opportunity");
+            newOpportunity["name"] = opportunity["name"];
+            newOpportunity["parentcontactid"] = opportunity["parentcontactid"];
+            newOpportunity["parentaccountid"] = opportunity["parentaccountid"];
+            newOpportunity["purchasetimeframe"] = opportunity["purchasetimeframe"];
+            newOpportunity["transactioncurrencyid"] = opportunity["transactioncurrencyid"];
+            newOpportunity["budgetamount"] = opportunity["budgetamount"];
+            newOpportunity["purchaseprocess"] = opportunity["purchaseprocess"];
+            newOpportunity["msdyn_forecastcategory"] = opportunity["msdyn_forecastcategory"];
+            newOpportunity["description"] = opportunity["description"];
+
+            return this.Service.Create(newOpportunity);
+        }
+
+        private Entity GetOpportunity(CodeActivityContext context)
+        {
+            return this.Service.Retrieve("opportunity", Guid.Parse(OpportunityId.Get(context)), new ColumnSet(true));
+        }
+    }
+}
---
+++ Dynacoop2023.Logistics.MyFirstPlugins/DynacoopISV/ActionCore.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynacoop2023.Logistics.MyFirstPlugins.DynacoopISV
+{
+    public abstract class ActionCore : CodeActivity
+    {
+        public IWorkflowContext WorkflowContext { get; set; }
+        public IOrganizationServiceFactory ServiceFactory { get; set; }
+        public IOrganizationService Service { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.WorkflowContext = context.GetExtension<IWorkflowContext>();
+            this.ServiceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            Service = this.ServiceFactory.CreateOrganizationService(WorkflowContext.UserId);
+
+            ExecuteAction(context);
+        }
+
+        public abstract void ExecuteAction(CodeActivityContext context);
+    }
+}
